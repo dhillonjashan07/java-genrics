f251d29c7ab65e9c20056d2291cd84be6f6a7fe4
dhillonjashan07 <102655916+dhillonjashan07@users.noreply.github.com>
2025-03-27 14:15:43 +0530

    [jashan] Add:flight-utility-model

---
+++ com/bridgelabz/flightscheduling/FlightUtility.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.flightscheduling;
+import java.util.List;
+
+class FlightUtility {
+    public static void displayFlightDetails(List<? extends Flight<?>> flights) {
+        for (Flight<?> flight : flights) {
+            flight.displayFlightInfo();
+        }
+    }
+}
